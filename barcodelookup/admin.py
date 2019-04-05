0f5b1de63016728f0e0a14a5a2a48ebe24ae46ec
girishk38 <girish.k38@yahoo.com>
2019-04-05 14:34:23 +0530

    Book model

--- barcodelookup/admin.py
+++ barcodelookup/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Book
 # Register your models here.
 
 # register Product database for admin panel
 admin.site.register(Product)
+admin.site.register(Book)
--- barcodelookup/models.py
+++ barcodelookup/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 
+# image url with image not found
+NOT_FOUND = "https://i.ibb.co/dj1qb0D/product-image-not-found.gif"
+
 # Create your models here.
 class Product(models.Model):
     """product details for barcode"""
 
-    barcode = models.CharField(max_length=48, unique=True)
-    name = models.CharField(max_length=100)
+    barcode = models.CharField(max_length=13, unique=True)
+    title = models.CharField(max_length=200, default='')
     description = models.TextField(default="")
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
-# class Book(models.Model):
-#     product = models.OneToOneField(Product)
+class Book(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    author = models.CharField(max_length=40)
+    # cover photo of book
+    cover = models.TextField(default=NOT_FOUND)
+    no_of_pages = models.IntegerField()
+    publisher = models.CharField(max_length=120)
+    publish_data = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.product.title
